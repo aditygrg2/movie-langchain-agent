4b924433b568966f1b0e47b6b67027eefa32cfe9
Aditya Garg <uber6707@gmail.com>
2024-02-18 18:28:15 +0530

    client url

--- agent/flask_app.py
+++ agent/flask_app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_socketio import SocketIO
 
 # CLIENT_URL = "http://localhost:3000"
-CLIENT_URL = "http://movie-langchain-agent.vercel.app"
+CLIENT_URL = "https://movie-langchain-agent.vercel.app"
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins=[CLIENT_URL], engineio_logger=True, ping_timeout=5, ping_interval=5)
